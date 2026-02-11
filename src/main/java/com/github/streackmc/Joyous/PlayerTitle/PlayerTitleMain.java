08e24000044130181229d0c0b7bbfea443b29a64
kdxiaoyi [MiPad7] <kdxiaoyi@outlook.com>
2026-02-11 17:52:31 +0800

    组件解耦和

--- src/main/java/com/github/streackmc/Joyous/PlayerTitle/PlayerTitleMain.java
+++ src/main/java/com/github/streackmc/Joyous/PlayerTitle/PlayerTitleMain.java
@@ -7,16 +7,12 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import com.github.streackmc.Joyous.Joyous;
 import com.github.streackmc.Joyous.logger;
 import com.github.streackmc.StreackLib.utils.MCColor;
 import com.github.streackmc.StreackLib.utils.SConfig;
 import com.github.streackmc.StreackLib.utils.SFile;
-
-import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 
 /**
  * 玩家称号
@@ -44,7 +40,7 @@ public class PlayerTitleMain {
   public static SConfig titleList;
 
   public static final void onEnable() {
-    PlaceholderService = new PlayerTitleMain.PlayerTitlePHAPI();
+    PlaceholderService = new PlayerTitlePHAPI();
     NAMES.PLAYER_USING_TITLE_NAMESPACED = new NamespacedKey(Joyous.plugin, NAMES.PLAYER_USING_TITLE);
     PlaceholderService.register();
     if (Files.notExists(CONF_PATH)) {
@@ -119,60 +115,6 @@ public class PlayerTitleMain {
       return false;
     } else {
       return true;
-    }
-  }
-
-  /** PlaceholderAPI注册类 */
-  public static final class PlayerTitlePHAPI extends PlaceholderExpansion {
-    public PlayerTitlePHAPI() {
-    }
-
-    @Override
-    @NotNull
-    public String getIdentifier() {
-      return "joyous"; // %joyous.xxx% 的前缀
-    }
-
-    @Override
-    @NotNull
-    public String getAuthor() {
-      return "kdxiaoyi & StreackMC Team";
-    }
-
-    @Override
-    @NotNull
-    public String getVersion() {
-      return Joyous.getVersion();
-    }
-
-    @Override
-    public boolean persist() {
-      return true; // 插件重载时不卸载此占位符
-    }
-
-    @Override
-    public boolean canRegister() {
-      return true; // 是否可以注册
-    }
-
-    @Override
-    @Nullable
-    public String onPlaceholderRequest(Player player, @NotNull String params) {
-      if (player == null)
-        return ""; // 显然没有玩家就没有称号
-
-      // %joyous.title% → 返回玩家称号
-      if (params.equalsIgnoreCase("name")) {
-        return getTitle(player);
-      }
-
-      // 这里是没删掉的教程示例代码，懒就没删
-      // %joyous.kills_<type>% → 带参数
-      // if (params.startsWith("kills_")) {
-      // String type = params.substring(6);
-      // return String.valueOf(Joyous.plugin.getKills(player, type));
-      // }
-      return null; // 未知占位符返回 null
     }
   }
 }
