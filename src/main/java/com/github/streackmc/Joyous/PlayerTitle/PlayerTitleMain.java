54f874be22bea577d88b9d3277f08ba163f387eb
kdxiaoyi [MiPad7] <kdxiaoyi@outlook.com>
2026-02-11 16:09:33 +0800

    实现 getTitle

--- src/main/java/com/github/streackmc/Joyous/PlayerTitle/PlayerTitleMain.java
+++ src/main/java/com/github/streackmc/Joyous/PlayerTitle/PlayerTitleMain.java
@@ -3,12 +3,16 @@ package com.github.streackmc.Joyous.PlayerTitle;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import com.github.streackmc.Joyous.Joyous;
 import com.github.streackmc.Joyous.logger;
+import com.github.streackmc.StreackLib.utils.MCColor;
 import com.github.streackmc.StreackLib.utils.SConfig;
 import com.github.streackmc.StreackLib.utils.SFile;
 
@@ -22,21 +26,33 @@ import me.clip.placeholderapi.expansion.PlaceholderExpansion;
  */
 public class PlayerTitleMain {
 
-  final static String CONF_NAME = "playerTitles.yml";
-  final static Path CONF_PATH = Joyous.dataPath.toPath().resolve(CONF_NAME);
+  final static Path CONF_PATH = Joyous.dataPath.toPath().resolve(NAMES.CONF_FILE);
+
+  public final static class NAMES {
+    /** 配置文件名 */
+    public final static String CONF_FILE = "playerTitles.yml";
+    /** 权限前缀 */
+    public final static String PERMISSION_PREFIX = "joyous.titles.";
+    /** 玩家正在使用的称号 */
+    public final static String PLAYER_USING_TITLE = "score";
+    /** 玩家正在使用的称号 */
+    static NamespacedKey PLAYER_USING_TITLE_NAMESPACED;
+  };
 
   public static PlayerTitlePHAPI PlaceholderService = null;
+  /** 称号列表 */
   public static SConfig titleList;
 
   public static final void onEnable() {
     PlaceholderService = new PlayerTitleMain.PlayerTitlePHAPI();
+    NAMES.PLAYER_USING_TITLE_NAMESPACED = new NamespacedKey(Joyous.plugin, NAMES.PLAYER_USING_TITLE);
     PlaceholderService.register();
     if (Files.notExists(CONF_PATH)) {
       try {
         logger.debug("检查到 %s 不存在，自动新建默认文件", CONF_PATH);
-        SFile.mv(Joyous.getResourceAsFile(CONF_NAME), CONF_PATH.toFile());
+        SFile.mv(Joyous.getResourceAsFile(NAMES.CONF_FILE), CONF_PATH.toFile());
       } catch (Exception e) {
-        logger.err("警告：无法写入 %s ： %s", CONF_NAME, e.getLocalizedMessage(), e);
+        logger.err("警告：无法写入 %s ： %s", NAMES.CONF_FILE, e.getLocalizedMessage(), e);
       }
     }
     titleList = new SConfig(CONF_PATH, "yml");
@@ -49,7 +65,38 @@ public class PlayerTitleMain {
 
   /** 获取玩家绑定的称号 */
   public static final String getTitle(Player player) {
-    return "";
+    PersistentDataContainer pdc = player.getPersistentDataContainer();
+
+    // 先获取玩家设置的称号
+    String titleId = pdc.get(NAMES.PLAYER_USING_TITLE_NAMESPACED, PersistentDataType.STRING);
+    if (titleId.equals(null) || titleId.equals("empty") || titleId.isEmpty()) {
+      // 没有设置就返回
+      return "";
+    }
+
+    // 检查是否过期
+    if (!player.hasPermission(NAMES.PERMISSION_PREFIX + titleId)) {
+      player.sendMessage(Joyous.i18n.get("titles.outdated"));
+      logger.info("玩家 [%s] 持有的称号 [%s] 已过期", player.getName(), titleId);
+      pdc.set(NAMES.PLAYER_USING_TITLE_NAMESPACED, PersistentDataType.STRING, "empty");
+      return "";
+    }
+
+    // 获取称号
+    String title = titleList.getString("titles." + titleId, "");
+    if (title.isEmpty()) {
+      title = "";
+      player.sendMessage(Joyous.i18n.get("titles.missing"));
+      logger.warn("找不到玩家 [%s] 持有的称号 [%s]", player.getName(), titleId);
+      pdc.set(NAMES.PLAYER_USING_TITLE_NAMESPACED, PersistentDataType.STRING, "empty");
+    } else {
+      title = String.format("&r&7%s&r&f%s&r&7%s&r",
+          Joyous.conf.getString("PlayerTitle.prefix", "「"),
+          title,
+          Joyous.conf.getString("PlayerTitle.suffix", "」"));
+      logger.debug("玩家 [%s] 持有的称号 [%s] 为 [%s]", player.getName(), titleId, title);
+    }
+    return MCColor.parse(title);
   }
 
   /** 设置称号 */
